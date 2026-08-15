f48e9de28a3b6b89fe72c6e8ec43b9c34bcf3067
PolluxÃÃ <moonbenchstudio@gmail.com>
2026-08-15 16:08:39 +0200

    chore(release): 1.3.0
    Passe la ligne 1.2.0-beta en version stable : la pile plugins,
    l'API IPC externe et l'effet PIPBOY sont fusionnes dans main.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.moonbench.bifrost"
         minSdk = 33
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.2.0-beta-7"
+        versionCode = 14
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
